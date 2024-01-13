fc31995df221ee69853ab4eed1eabc17065f98b0
XzoRit <xzorit@hotmail.com>
2024-01-13 20:03:50 +0100

    add tests and implementation for generic exception type

--- lib/lib/exception.hpp
+++ lib/lib/exception.hpp
@@ -1,8 +1,49 @@
 #pragma once
 
+#include <lib/source_location.hpp>
+
+#include <string>
+#include <utility>
+
 namespace xzr::error
 {
+template <class Data>
 class exception
 {
+  public:
+    explicit exception(
+        std::string msg,
+        Data d,
+        ext::source_location sl = ext::source_location::current())
+        : m_msg{std::move(msg)}
+        , m_data{std::move(d)}
+        , m_sl{sl}
+    {
+    }
+
+    const std::string& what() const
+    {
+        return m_msg;
+    }
+
+    std::string& what()
+    {
+        return m_msg;
+    }
+
+    const Data& data() const
+    {
+        return m_data;
+    }
+
+    ext::source_location where() const
+    {
+        return m_sl;
+    }
+
+  private:
+    std::string m_msg;
+    Data m_data;
+    ext::source_location m_sl;
 };
 }
--- lib/lib/exception.test.cpp
+++ lib/lib/exception.test.cpp
@@ -1,4 +1,5 @@
 #include <lib/exception.hpp>
+#include <lib/source_location.hpp>
 
 #include <boost/test/unit_test.hpp>
 
@@ -6,6 +7,8 @@ namespace
 {
 struct test_exception
 {
+    using E = ::xzr::error::exception<int>;
+
     test_exception()
     {
     }
@@ -19,6 +22,19 @@ BOOST_FIXTURE_TEST_SUITE(exception_tests, test_exception)
 
 BOOST_AUTO_TEST_CASE(ctor)
 {
+    const auto e{E{"msg", 1}};
+
+    BOOST_TEST(e.what() == "msg");
+    BOOST_TEST(e.where().line() == __LINE__ - 3);
+    BOOST_TEST(e.data() == 1);
+}
+
+BOOST_AUTO_TEST_CASE(mut_msg)
+{
+    auto e{E{"msg", 1}};
+    e.what() += " mut";
+
+    BOOST_TEST(e.what() == "msg mut");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
