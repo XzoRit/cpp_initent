f5d96ad98499815e8f67231cc554e3b58b9b2979
Matthias Merkers <xzorit@hotmail.com>
2024-02-04 15:36:01 +0100

    throw exception by streaming message and adding a source location

--- lib/lib/thrw.hpp
+++ lib/lib/thrw.hpp
@@ -3,6 +3,7 @@
 #include <lib/source_location.hpp>
 
 #include <sstream>
+#include <type_traits>
 #include <utility>
 
 namespace xzr::error
@@ -70,5 +71,76 @@ auto throw_it(
     ::xzr::ext::source_location sl = ::xzr::ext::source_location::current())
 {
     return collect<E>{sl};
+}
+
+template <class E>
+class [[maybe_unused]] thrw_msg
+{
+  private:
+    using source_location = ::xzr::ext::source_location;
+
+  public:
+    thrw_msg() = default;
+    explicit thrw_msg(const char* msg)
+    {
+        m_stream << msg;
+    }
+    explicit thrw_msg(const char* msg, source_location sl)
+        : m_sl{sl}
+    {
+        m_stream << msg;
+    }
+#if defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable : 4722)
+#endif
+    [[noreturn]] ~thrw_msg() noexcept(false)
+    {
+        if constexpr (std::is_constructible_v<E, const char*, source_location>)
+        {
+            throw E{m_stream.str().c_str(), m_sl};
+        }
+        else
+        {
+            throw E{m_stream.str().c_str()};
+        }
+    }
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#endif
+
+  private:
+    template <class FormatStreamable>
+    friend thrw_msg&& operator<<(thrw_msg&& r, const FormatStreamable& it)
+    {
+        r.m_stream << it;
+        return std::move(r);
+    }
+
+    std::stringstream m_stream{};
+    source_location m_sl;
+};
+
+template <class E>
+auto throw_msg()
+{
+    return thrw_msg<E>{};
+}
+
+template <class E>
+auto throw_msg(
+    const char* msg,
+    ::xzr::ext::source_location sl = ::xzr::ext::source_location::current())
+{
+    if constexpr (std::is_constructible_v<E,
+                                          const char*,
+                                          ::xzr::ext::source_location>)
+    {
+        return thrw_msg<E>{msg, sl};
+    }
+    else
+    {
+        return thrw_msg<E>{msg};
+    }
 }
 }
--- lib/lib/thrw.test.cpp
+++ lib/lib/thrw.test.cpp
@@ -1,15 +1,31 @@
-#include <lib/exception.hpp>
+#include <lib/source_location.hpp>
 #include <lib/thrw.hpp>
 
 #include <boost/test/unit_test.hpp>
 
+#include <stdexcept>
+
 namespace
 {
-using ::xzr::error::throw_it;
+using ::xzr::error::throw_msg;
 
 struct test_thrw
 {
-    using E = ::xzr::error::exception<int>;
+    using source_location = ::xzr::ext::source_location;
+    struct E : public std::runtime_error
+    {
+        E(const char* msg, source_location sl = source_location::current())
+            : std::runtime_error{msg}
+            , m_sl{sl}
+        {
+        }
+        source_location where() const
+        {
+            return m_sl;
+        }
+        const char* m_msg;
+        source_location m_sl;
+    };
 
     test_thrw()
     {
@@ -22,23 +38,61 @@ struct test_thrw
 
 BOOST_FIXTURE_TEST_SUITE(thrw_tests, test_thrw)
 
-BOOST_AUTO_TEST_CASE(throw_catch_with_data)
+BOOST_AUTO_TEST_CASE(throw_simple_msg)
 {
-
     try
     {
-        throw_it<E>()("msg", 1) << " extra msg " << 2;
+        throw_msg<std::runtime_error>("msg");
+    }
+    catch (const std::runtime_error& e)
+    {
+        BOOST_TEST(e.what() == "msg");
+    }
+}
+
+BOOST_AUTO_TEST_CASE(throw_msg_with_streaming)
+{
+    try
+    {
+        throw_msg<std::runtime_error>("msg: ") << 1 << " msg: " << 2;
+    }
+    catch (const std::runtime_error& e)
+    {
+        BOOST_TEST(e.what() == "msg: 1 msg: 2");
+    }
+}
+
+BOOST_AUTO_TEST_CASE(throw_msg_with_location)
+{
+    try
+    {
+        throw_msg<E>("msg: ") << 1 << " msg: " << 2;
     }
     catch (const E& e)
     {
-        BOOST_TEST(e.where().line() == __LINE__ - 4);
+        BOOST_TEST(e.what() == "msg: 1 msg: 2");
+        BOOST_TEST(e.where().line() == __LINE__ - 5);
         BOOST_TEST(e.where().file_name() == __FILE__);
         BOOST_TEST(e.where().function_name() == __func__);
-        BOOST_TEST(e.what() == "msg extra msg 2");
-        BOOST_TEST(e.str() == "msg extra msg 2");
-        BOOST_TEST(e.data() == 1);
     }
 }
+
+// BOOST_AUTO_TEST_CASE(throw_msg_with_location_and_data)
+// {
+//     try
+//     {
+//         throw_it<E>()("msg", 1) << " extra msg " << 2;
+//     }
+//     catch (const E& e)
+//     {
+//         BOOST_TEST(e.where().line() == __LINE__ - 4);
+//         BOOST_TEST(e.where().file_name() == __FILE__);
+//         BOOST_TEST(e.where().function_name() == __func__);
+//         BOOST_TEST(e.what() == "msg extra msg 2");
+//         BOOST_TEST(e.str() == "msg extra msg 2");
+//         BOOST_TEST(e.data() == 1);
+//     }
+// }
 
 BOOST_AUTO_TEST_SUITE_END()
 }
